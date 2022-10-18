12e0814277b5d77e1d66d21ace31043ad9979284
Merugu Venkatesh <89741493+MeruguVenkatesh2766@users.noreply.github.com>
2022-10-18 12:42:45 +0530

    Update Dashboard.js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard=()=>{
     return(
         <div className="Dashboard">
             <h2>Dashboard</h2>
-            <h4>Name: {name}</h4>
+            <h4>Name: {fname} {lname}</h4>
             <h4>Email: {email}</h4>
             <h4>Skills: c++,java</h4>
         </div>
